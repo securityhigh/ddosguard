a308b51a09f17c818519b18370de131a0cd141ff
Security High <45529919+securityhigh@users.noreply.github.com>
2020-11-30 19:11:54 +0700

    Add files via upload

---
+++ net.sh
@@ -0,0 +1,8 @@
+netstat -na | grep ":443\ " | wc -l
+netstat -na | grep ":80\ " | wc -l
+netstat -na | grep ":8080\ " | wc -l
+netstat -na | grep ":8181\ " | wc -l
+netstat -na | grep ":8443\ " | wc -l
+netstat -na | grep ":21\ " | wc -l
+netstat -na | grep ":22\ " | wc -l
+netstat -na | grep ":0\ " | wc -l
